9f0fed094978824dfd6774ddf1bbd5ad0d00e1cb
annlen <annsofi.len@gmail.com>
2023-02-10 22:50:31 +0200

    add routes for insert and delete car

--- backend/routes/cars.js
+++ backend/routes/cars.js
@@ -37,5 +37,25 @@ router.post('/update-availability', async (req, res) => {
     }
 })
 
+router.post('/delete-car', async (req, res) => {
+    try {
+        let isok = await mongoDBService.deleteCar(req.body.regnbr)
+        res.send(isok)
+    } catch (error) {
+        console.log(error)
+        res.send(false)
+    }
+})
+
+router.post('/insert-car', async (req, res) => {
+    try {
+        let isok = await mongoDBService.insertCar(req.body.regnbr, req.body.brand, req.body.kilometers, req.body.inne)
+        res.send(isok)
+    } catch (error) {
+        console.log(error)
+        res.send(false)
+    }
+})
+
 
 module.exports = router
